bc269531df64786ebcdedc7662719fd95edf048f
Andrey <corhal@yandex.ru>
2016-08-07 20:47:02 +0300

    Add files via upload

---
+++ Assets/Scripts/Arrow.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Arrow : MonoBehaviour {
+
+	public float Force;
+
+	private Rigidbody myRigidbody;
+	private bool isFlying;
+
+	void Awake() {
+		myRigidbody = GetComponent<Rigidbody> ();
+	}
+
+	void Start() {
+		isFlying = true;
+		myRigidbody.AddForce (new Vector3(transform.forward.x * Force, transform.forward.y * Force, transform.forward.z * Force));
+	}
+
+	void Update() {
+		if (isFlying) {
+			transform.rotation = Quaternion.LookRotation (myRigidbody.velocity);
+		}
+		//Debug.Log (transform.rotation);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		//if (other.GetComponent<Arrow>() == null) {
+			isFlying = false;
+			myRigidbody.velocity = Vector3.zero;
+			Destroy (myRigidbody);
+			//myRigidbody.useGravity = false;
+		//}
+	}
+}
---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shooter : MonoBehaviour {
+
+	public GameObject ShootPoint;
+	public GameObject ArrowPrefab;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			Instantiate (ArrowPrefab, ShootPoint.transform.position, ShootPoint.transform.rotation);
+		}	
+	}
+}
